be0b55b06bfdd002d85b6fcc72aaced1676c013f
Damola Olusakin <olusakinadedamola@gmail.com>
2021-10-10 11:05:31 +0100

    installed react navigation

--- App.js
+++ App.js
@@ -6,13 +6,16 @@ import Quotation from "./screens/Quotation";
 import Basket from "./screens/Basket";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <Provider store={store}>
+      <NavigationContainer>
     <SafeAreaView style={styles.container}>
       <Basket />
     </SafeAreaView>
+    </NavigationContainer>
     </Provider>
   );
 }
