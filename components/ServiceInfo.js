eee08ae4fd1ac3c7c5736f520d37dcf507a1e971
Damola Olusakin <olusakinadedamola@gmail.com>
2021-10-26 11:46:47 +0100

    start service component

---
+++ components/ServiceInfo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+
+const ServiceInfo = () => {
+  return (
+    <View>
+      <Text>Car Servicing</Text>
+    </View>
+  )
+}
+
+export default ServiceInfo
+
+const styles = StyleSheet.create({})
