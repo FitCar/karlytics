447d1c581a86d4d20d6d402e8a7ed93505764e75
Ebireni David <ebirenidavid@gmail.com>
2022-02-08 15:28:00 +0100

    added code to hide warning logs temporarily

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
-
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Routes from './navigation/index';
-import { StyleSheet, View } from "react-native";
+import { LogBox } from "react-native";
 import CarCard from "./components/CarCard";
 import CarRegisteration from "./screens/CarRegisteration";
 import { Picker } from "@react-native-community/picker";
 import CarMakes from "./screens/CarMakes";
 
-
-
-
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs()
 
 export default function App() {
 
