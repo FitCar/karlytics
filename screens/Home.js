6c241aca097d7fb49cdbd15df51e9015d421d6f4
Ebireni David <ebirenidavid@gmail.com>
2022-02-16 14:14:33 +0100

    made the progress bar dynamic with service date supplied

--- screens/Home.js
+++ screens/Home.js
@@ -15,14 +15,11 @@ import tw from "tailwind-react-native-classnames";
 import AddCar from "../components/AddCar";
 import HealthCard from "../components/HealthCard";
 import ServiceButton from "../components/ServiceButton";
-import { IconButton } from "../components";
 import Firebase from "../config/firebase";
 import { AuthenticatedUserContext } from "../navigation/AuthenticatedUserProvider";
 import ServiceInfo from "../components/ServiceInfo";
 import { useSelector, useDispatch } from "react-redux";
 import { selectLastServiceDate } from "../slices/carSlice";
-import { formatDistanceToNow } from "date-fns";
-import CarCard from "../components/CarCard";
 import { getCars } from '../slices/carSlice'
 
 const firestore = Firebase.firestore();
@@ -31,7 +28,6 @@ const Home = () => {
   const [loading, setLoading] = useState(true); // Set loading to true on component mount
   const lastServiceDate = useSelector(selectLastServiceDate);
   const [usersFullname, setusersFullname] = useState(null);
-  const [disableReq, setdisableReq] = useState(false)
 
   const navigation = useNavigation();
   const { user } = useContext(AuthenticatedUserContext);
@@ -59,49 +55,25 @@ const Home = () => {
         setLoading(false);
       });
 
-      if(current_car){
-        setdisableReq(false)
-      }else{
-        setdisableReq(true)
-      }
   }, []);
 
-    useEffect(() => {
-      const fetchuserData = async () => {
-        await firestore.collection("users").doc(user.uid).get()
-              .then(doc => setusersFullname(doc.data().name))
-              .catch(error => console.log(error))
-      }
-      
-      fetchuserData()
-    }, [user])
+  useEffect(() => {
+    const fetchuserData = async () => {
+      await firestore.collection("users").doc(user.uid).get()
+            .then(doc => setusersFullname(doc.data().name))
+            .catch(error => console.log(error))
+    }
+    
+    fetchuserData()
+  }, [user])
   
 
   const handleServiceButton = (route) => {
-    if(disableReq) return alert("Select a car to access these services")
+    if(!current_car) return alert("Select a car to access these services")
     
     return navigation.navigate(route)
   }
 
-  const dt =
-    cars.length !== 0 ? new Date(cars[0].nextServiceDate) : new Date();
-  const lstDt =
-    cars.length !== 0 ? new Date(cars[0].lastServiceDate) : new Date();
-  // console.log(dt);
-  const duration = formatDistanceToNow(dt);
-  // console.log(duration);
-
-  const now = new Date();
-  const one_day = 1000 * 60 * 60 * 24;
-  const totalDays = (Math.round(dt.getTime() - lstDt.getTime()) / one_day).toFixed(0);
-  
-  console.log(totalDays);
-  
-  const leftDays = (Math.round(dt.getTime() - new Date().getTime()) / one_day).toFixed(0);
-  console.log(leftDays);
-
-  const progressLeft = leftDays / totalDays;
-  console.log(progressLeft);
   
   return (
     <View style={tw`bg-white pt-10`}>
@@ -131,14 +103,17 @@ const Home = () => {
           </View>
 
           {cars.length !== 0 ? (
-            <View style={tw`px-3 mt-5 mb-3`}>
+            <View style={tw`px-4 mt-5 mb-3`}>
               {
-                current_car ? 
-                <ServiceInfo
-                  serviceDate={cars[0].nextServiceDate}
-                  duration={duration}
-                  progressLeft={progressLeft}
-                />
+                current_car ?
+                <View>
+                  <Text style={[tw`text-2xl font-semibold mb-5`, { color: "#2bced6" }]}>{current_car.Make} {current_car.Model}</Text>
+           
+                  <ServiceInfo
+                    current_car={current_car}
+                  />
+                </View> 
+                
                 :
                 <Text style={tw`text-xl text-gray-600 font-medium`}>Select a car from your garage</Text>
               }
