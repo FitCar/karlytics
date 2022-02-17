accceab702c79cfdfc881a40f11e3926dca016bc
Damola Olusakin <olusakinadedamola@gmail.com>
2022-02-17 12:13:11 +0100

    added remove from basket functionality for quotation items

--- components/BasketCard.js
+++ components/BasketCard.js
@@ -2,50 +2,78 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import { Icon } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
-import AwesomeAlert from 'react-native-awesome-alerts';
+import AwesomeAlert from "react-native-awesome-alerts";
 import { removeFromBasket } from "../slices/carSlice";
 import { useDispatch } from "react-redux";
 
-const ChooseImageForPlan = ({ name }) =>{
-  
-  if(name === 'Membership') return <Image source={require("../assets/icons/membership.png")} style={{ resizeMode: "contain", height: 40 }} />
-  
-  if(name === 'Insurnce') return <Image source={require("../assets/icons/insurance.png")} style={{ resizeMode: "contain", height: 40 }} />
- 
-  if(name === 'Maintenance') return <Image source={require("../assets/icons/maintain.png")} style={{ resizeMode: "contain", height: 40 }} />
-  
-  return <Image source={require("../assets/icons/healthplan.png")} style={{ resizeMode: "contain", height: 40 }} />
-} 
+const ChooseImageForPlan = ({ name }) => {
+  if (name === "Membership")
+    return (
+      <Image
+        source={require("../assets/icons/membership.png")}
+        style={{ resizeMode: "contain", height: 40 }}
+      />
+    );
+
+  if (name === "Insurnce")
+    return (
+      <Image
+        source={require("../assets/icons/insurance.png")}
+        style={{ resizeMode: "contain", height: 40 }}
+      />
+    );
+
+  if (name === "Maintenance")
+    return (
+      <Image
+        source={require("../assets/icons/maintain.png")}
+        style={{ resizeMode: "contain", height: 40 }}
+      />
+    );
+
+  return (
+    <Image
+      source={require("../assets/icons/healthplan.png")}
+      style={{ resizeMode: "contain", height: 40 }}
+    />
+  );
+};
 
 const BasketCard = ({ description, qty, total, unitPrice, details, index }) => {
-  
-  const [showAlert, setshowAlert] = useState(false)
-  const dispatch = useDispatch()
+  const [showAlert, setshowAlert] = useState(false);
+  const dispatch = useDispatch();
 
   const confirmRemove = () => {
-    dispatch(removeFromBasket(index))
-    return setshowAlert(false)
-  }
+    dispatch(removeFromBasket(index));
+    return setshowAlert(false);
+  };
 
+  if (details)
+    return (
+      <TouchableOpacity
+        style={tw`bg-white mx-4 rounded-xl mb-5 p-4 shadow-xl`}
+        key={index}
+        onLongPress={() => setshowAlert(true)}
+      >
+        <View style={tw`flex-row justify-start h-max items-center`}>
+          <ChooseImageForPlan name={details.plan.Name} />
 
-  if(details) return (
-    <TouchableOpacity 
-      style={tw`bg-white mx-4 rounded-xl mb-5 p-4 shadow-xl`} key={index}
-      onLongPress={() => setshowAlert(true)}
-    >
-      <View style={tw`flex-row justify-start h-max items-center`}>
-        <ChooseImageForPlan name={details.plan.Name} />
-        
-        <View style={tw`ml-5`}>
-          <Text style={tw`mb-2`}><Text style={tw`font-semibold`}>{details.plan.type}</Text> package for <Text style={tw`font-semibold`}>{details.plan.Name}</Text> plan</Text>
-          <Text>Make - {details.Make}</Text>
-          <Text>Model - {details.Model}</Text>
-          <Text style={tw`font-semibold mt-2`}>Cost: {details.plan.price}</Text>  
+          <View style={tw`ml-5`}>
+            <Text style={tw`mb-2`}>
+              <Text style={tw`font-semibold`}>{details.plan.type}</Text> package
+              for <Text style={tw`font-semibold`}>{details.plan.Name}</Text>{" "}
+              plan
+            </Text>
+            <Text>Make - {details.Make}</Text>
+            <Text>Model - {details.Model}</Text>
+            <Text style={tw`font-semibold mt-2`}>
+              Cost: {details.plan.price}
+            </Text>
+          </View>
         </View>
-      </View>
 
-      {/* renders the alert that is shown before item is removed from basket */}
-      <AwesomeAlert
+        {/* renders the alert that is shown before item is removed from basket */}
+        <AwesomeAlert
           show={showAlert}
           title={"remove from basket?"}
           message={`Are you sure you want to remove ${details.plan.Name} for your ${details.Make}`}
@@ -59,22 +87,40 @@ const BasketCard = ({ description, qty, total, unitPrice, details, index }) => {
           onCancelPressed={() => setshowAlert(false)}
           onConfirmPressed={() => confirmRemove()}
         />
-    </TouchableOpacity>
-  )
+      </TouchableOpacity>
+    );
   return (
-    <View style={tw`bg-white mx-4 rounded-xl mb-5 p-4 shadow-xl`} key={index}>
+    <TouchableOpacity
+      style={tw`bg-white mx-4 rounded-xl mb-5 p-4 shadow-xl`}
+      key={index}
+      onLongPress={() => setshowAlert(true)}
+    >
       <View style={tw`flex-row h-12 items-center`}>
         <Icon style={tw`flex-1 mr-10`} name="gas-pump" type="font-awesome-5" />
-        
+
         <View>
           <Text>{description}</Text>
           <Text>Quantity: {qty}</Text>
           <Text>Unit Price: {unitPrice}</Text>
-          <Text>Total: {total}</Text>  
+          <Text>Total: {total}</Text>
         </View>
       </View>
-    </View>
-  )
+      <AwesomeAlert
+          show={showAlert}
+          title={"remove from basket?"}
+          message={`Are you sure you want to remove ${description}`}
+          closeOnTouchOutside={true}
+          closeOnHardwareBackPress={false}
+          showCancelButton={true}
+          showConfirmButton={true}
+          cancelText="No, cancel"
+          confirmText="Yes, delete it"
+          confirmButtonColor="#DD6B55"
+          onCancelPressed={() => setshowAlert(false)}
+          onConfirmPressed={() => confirmRemove()}
+        />
+    </TouchableOpacity>
+  );
 };
 
 export default BasketCard;
