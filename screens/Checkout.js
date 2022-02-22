262dded5f7d76a04357568fbae8bd1a5c409a5b4
Ebireni David <ebirenidavid@gmail.com>
2022-02-22 21:48:30 +0100

    set up handle success function to add plans to firestore

--- screens/Checkout.js
+++ screens/Checkout.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, Button } from 'react-native'
 import  { Paystack }  from 'react-native-paystack-webview';
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
 import { selectGrandTotal } from "../slices/carSlice";
+import { AuthenticatedUserContext } from "../navigation/AuthenticatedUserProvider";
+import Firebase from '../config/firebase';
+
+const firestore = Firebase.firestore()
+
 
 const Checkout = () => {
-
   const navigation = useNavigation()
   const grandTotal = useSelector(selectGrandTotal)
+  const { basket } = useSelector(state => state.car)
+
+  const { user } = useContext(AuthenticatedUserContext);
+
+  const handleSuccess = () =>{
+    const plansRef = firestore.collection("Plans")
+              .doc(user.uid)
+              .collection("Plans")
+
+    const plans_in_basket = basket.filter(item => "plan" in item)
+
+    plans_in_basket.forEach(async (plan) => {
+      await plansRef.doc().set({ carId: plan.key,  plan: plan.plan, garage_id: plan.garageId})
+    })
+
+    return navigation.navigate('Home')
+  }
+   
 
   return (
-    <View style={{flex: 1}}>
+    <View>
       <Paystack  
         paystackKey="pk_test_0adc58258fd0cb5fc19574e4e371ab721109d35d"
         amount={grandTotal}
@@ -20,14 +42,11 @@ const Checkout = () => {
         onCancel={(e) => {
           // handle response here
         }}
-        onSuccess={(res) => {
-          // handle response here
-          console.log(res)
-          navigation.navigate('Home')
-        }}
+        onSuccess={() => handleSuccess()}
+        SafeAreaViewContainer={{}}
         autoStart={true}
       />
-      
+      <Text>Restart</Text>
     </View>
   )
 }
