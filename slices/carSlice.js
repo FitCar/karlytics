853798d3577523ae36373d591e6cc882ea9480f5
Ebireni David <ebirenidavid@gmail.com>
2022-02-23 20:44:18 +0100

    added reset action for clearing basket

--- slices/carSlice.js
+++ slices/carSlice.js
@@ -41,6 +41,9 @@ export const carSlice = createSlice({
     removeFromBasket: (state, action) => {
         state.basket = state.basket.filter((carPlan,index) => index+1 !== action.payload)
     },
+    resetBasket: (state, action) => {
+        state.basket = []
+    },
     setGrandTotal: (state, action) => {
       state.grandTotal = action.payload;
     },
@@ -59,7 +62,7 @@ export const carSlice = createSlice({
   },
 });
 
-export const { getCars, addCar, removeCar, addMake, addModel, setRequestId, addToBasket, removeFromBasket,  setGrandTotal, setLastServiceDate, setNextDate, setCurrentCar, getPlans } = carSlice.actions;
+export const { getCars, addCar, removeCar, addMake, addModel, setRequestId, addToBasket, removeFromBasket, resetBasket,  setGrandTotal, setLastServiceDate, setNextDate, setCurrentCar, getPlans } = carSlice.actions;
 
 export const selectCar = (state) => state.car.cars;
 export const selectMake = (state) => state.car.make;
