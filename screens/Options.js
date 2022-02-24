316e24c8d7453475ad03f90a85d160ad80833e3f
Damola Olusakin <olusakinadedamola@gmail.com>
2022-02-24 21:05:02 +0100

    restyled options page

--- screens/Options.js
+++ screens/Options.js
@@ -20,18 +20,22 @@ const Options = () => {
 
 
   return (
-    <View style={tw`mt-20`}>
-      <Text style={tw`mb-5`}>Profile</Text>
-      <Text style={tw`mb-5`}>Help and Support</Text>
-      <Text style={tw`mb-5`}>Privacy policy</Text>
-      <View style={tw`flex-row mb-5`}>
+    <View style={tw`mt-20 ml-5 mr-5`}>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Profile</Text>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Payment</Text>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Change password</Text>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Contact us</Text>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Help and Support</Text>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Terms and Conditions</Text>
+      <Text style={tw`mb-5 font-bold border-b-gray-50 border-b`}>Privacy policy</Text>
+      <View style={tw`flex-row mb-5 border-b-gray-50 border-b`}>
           <IconButton
             name="logout"
             size={24}
             color="#2bced6"
             onPress={handleSignOut}
           />
-          <Text style={tw`ml-5`}>Logout</Text>
+          <Text style={tw`ml-5 font-bold`}>Logout</Text>
         </View>
     </View>
   );
