7f8f1897f875af809e4d3253104a3058bb0fc6b2
Ebireni David <ebirenidavid@gmail.com>
2022-05-31 23:28:39 +0100

    modified components to suit the tests

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import LoginScreen from '../screens/Login';
-import RegisterScreen from '../screens/Register';
-import OnBoardScreen from '../screens/OnBoardScreen'
+import LoginScreen from "../screens/Login";
+import RegisterScreen from "../screens/Register";
+import OnBoardScreen from "../screens/OnBoardScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AuthStack() {
   return (
-    <Stack.Navigator initialRouteName='Welcome' screenOptions={{ headerShown: false }}>
-      
-      <Stack.Screen name='Welcome' component={OnBoardScreen} />
-      <Stack.Screen name='Login' component={LoginScreen} />
-      <Stack.Screen name='Register' component={RegisterScreen} />
-      
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Welcome" component={OnBoardScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 }
